7bed694b152e92b5133975df71eadc848a008c2c
Dmitrii <38377296+DmRk-Vl@users.noreply.github.com>
2019-09-25 21:25:30 +0700

    Update slackAPI.js

--- back/slackAPI.js
+++ back/slackAPI.js
@@ -2,8 +2,8 @@ const { RTMClient } = require('@slack/rtm-api');
 const { createMessageAdapter } = require('@slack/interactive-messages');
 const { WebClient } = require('@slack/web-api');
 
-const token = 'xoxb-759067883878-754448417585-wXBFerJRdJTbTuaIXgD11gIU';//process.env.SLACK_BOT_TOKEN; //'xoxb-759067883878-754448417585-wXBFerJRdJTbTuaIXgD11gIU';
-const slackSigningSecret = '0c15f8573db6e33929f00e545e681139';//process.env.SLACK_SIGNING_SECRET; //'0c15f8573db6e33929f00e545e681139';
+const token = process.env.SLACK_BOT_TOKEN;
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 
 const rtm = new RTMClient(token);
 const web = new WebClient(token);
